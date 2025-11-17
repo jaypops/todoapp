e879484cdda7861a7aa82c87e2fc0325f8238113
Timothy-TO <teniolatimothy@gmail.com>
2025-11-17 06:35:09 +0100

    updated

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
+import { createSettingsStyles } from "@/assets/styles/setting.styles";
+import DangerZone from "@/components/DangerZone";
+import Preferences from "@/components/Preferences";
+import ProgressStats from "@/components/ProgressStats";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Settings() {
+  const { colors } = useTheme();
+  const settingsStyles = createSettingsStyles(colors);
   return (
-    <View>
-      <Text>Settings Screen</Text>
+    <View style={{ flex: 1, backgroundColor: colors.backgrounds }}>
+      <LinearGradient
+        colors={colors.gradients.background}
+        style={settingsStyles.container}
+      >
+        <SafeAreaView style={settingsStyles.safeArea}>
+          <View style={settingsStyles.header}>
+            <View style={settingsStyles.titleContainer}>
+              <LinearGradient
+                colors={colors.gradients.primary}
+                style={settingsStyles.iconContainer}
+              >
+                <Ionicons name="settings" size={28} color="#ffffff" />
+              </LinearGradient>
+              <Text style={settingsStyles.title}>Settings</Text>
+            </View>
+          </View>
+          <ScrollView
+            style={settingsStyles.scrollView}
+            contentContainerStyle={settingsStyles.content}
+            showsVerticalScrollIndicator={false}
+          >
+            <ProgressStats />
+            <Preferences />
+            <DangerZone />
+          </ScrollView>
+        </SafeAreaView>
+      </LinearGradient>
     </View>
   );
 }
---
+++ components/Preferences.tsx
@@ -0,0 +1,74 @@
+import { createSettingsStyles } from "@/assets/styles/setting.styles";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import { Switch, Text, View } from "react-native";
+
+export default function Preferences() {
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotifiEnabled, setIsNotifiEnabled] = useState(true);
+  const { colors, isDarkMode, toggleDarkMode } = useTheme();
+  const settingsStyles = createSettingsStyles(colors);
+  return (
+    <LinearGradient
+      colors={colors.gradients.surface}
+      style={settingsStyles.section}
+    >
+      <Text style={settingsStyles.sectionTitle}>Preferences</Text>
+      <View style={settingsStyles.settingItem}>
+        <View style={settingsStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.primary}
+            style={settingsStyles.settingIcon}
+          >
+            <Ionicons name="moon" size={18} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.settingText}>Dark Mode</Text>
+        </View>
+        <Switch
+          value={isDarkMode}
+          onValueChange={toggleDarkMode}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.primary }}
+        />
+      </View>
+
+      <View style={settingsStyles.settingItem}>
+        <View style={settingsStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.warning}
+            style={settingsStyles.settingIcon}
+          >
+            <Ionicons name="notifications" size={18} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.settingText}>Notifications</Text>
+        </View>
+        <Switch
+          value={isNotifiEnabled}
+          onValueChange={() => setIsNotifiEnabled(!isNotifiEnabled)}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.warning }}
+        />
+      </View>
+
+      <View style={settingsStyles.settingItem}>
+        <View style={settingsStyles.settingLeft}>
+          <LinearGradient
+            colors={colors.gradients.success}
+            style={settingsStyles.settingIcon}
+          >
+            <Ionicons name="moon" size={18} color="#fff" />
+          </LinearGradient>
+          <Text style={settingsStyles.settingText}>Auto Sync</Text>
+        </View>
+        <Switch
+          value={isAutoSync}
+          onValueChange={() => setIsAutoSync(!isAutoSync)}
+          thumbColor={"#fff"}
+          trackColor={{ false: colors.border, true: colors.success }}
+        />
+      </View>
+    </LinearGradient>
+  );
+}
